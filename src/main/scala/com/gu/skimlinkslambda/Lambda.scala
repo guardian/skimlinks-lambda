5a60194da15bdc76ed9812f40677056f83955297
Philip McMahon <philip.mcmahon@guardian.co.uk>
2018-04-12 15:46:31 +0100

    Parameterise key for domains file in s3, improve logging.

--- src/main/scala/com/gu/skimlinkslambda/Lambda.scala
+++ src/main/scala/com/gu/skimlinkslambda/Lambda.scala
@@ -14,7 +14,8 @@ class LambdaInput() {
   def setName(theName: String): Unit = name = theName
 }
 
-case class Config(app: String, stack: String, stage: String, skimlinksApiKey: String, skimlinksAccountId: String, bucket: String) {
+case class Config(app: String, stack: String, stage: String, skimlinksApiKey: String, skimlinksAccountId: String,
+  bucket: String, domainsKey: String) {
   override def toString: String = s"App: $app, Stack: $stack, Stage: $stage\n"
 }
 
@@ -30,8 +31,9 @@ object Lambda {
       apiKey <- Option(System.getenv("SkimlinksApiKey"))
       accountId <- Option(System.getenv("SkimlinksAccountId"))
       domainsBucket <- Option(System.getenv("DomainsBucket"))
+      domainsKey <- Option(System.getenv("Domainskey"))
     } yield {
-      Config(app, stack, stage, apiKey, accountId, domainsBucket)
+      Config(app, stack, stage, apiKey, accountId, domainsBucket, domainsKey)
     }
   }
 
@@ -48,7 +50,12 @@ object Lambda {
   }
 
   def process(config: Config): Unit = {
-    val uploadResult = SkimlinksAPI.getDomains(config.skimlinksApiKey, config.skimlinksAccountId).map(S3.uploadDomainsToS3(_, config.bucket))
+    logger.info("Fetching skimlinks domains")
+    val domainsResult = SkimlinksAPI.getDomains(config.skimlinksApiKey, config.skimlinksAccountId)
+    val uploadResult = domainsResult.map{ domains =>
+      logger.info(s"Uploading ${domains.length} domains to S3")
+      S3.uploadDomainsToS3(domains, config.bucket)
+    }
     if (uploadResult.isLeft) logger.error(s"Failed to fetch domains ", uploadResult.left.get)
   }
 }
@@ -57,9 +64,9 @@ object TestIt {
   def main(args: Array[String]): Unit = {
     args.foreach(println)
     if (args.length < 3) {
-      println("Usage: run <apikey> <accountid> <bucket>")
+      println("Usage: run <apikey> <accountid> <bucket> <key>")
     } else {
-      Lambda.process(Config("test", "test", "test", args(0), args(1), args(3)))
+      Lambda.process(Config("test", "test", "test", args(0), args(1), args(3), args(4)))
     }
   }
 }
