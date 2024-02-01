fc31f5bfa6371bfa291eb7148af275ed88dd2e7d
Alina Boghiu <alina.boghiu@gmail.com>
2024-02-01 14:47:55 +0000

    Update the Skimlinks API to v4

--- src/main/scala/com/gu/skimlinkslambda/SkimlinksAPI.scala
+++ src/main/scala/com/gu/skimlinkslambda/SkimlinksAPI.scala
@@ -1,21 +1,41 @@
 package com.gu.skimlinkslambda
 
-import scalaj.http._
-import io.circe._
 import io.circe.parser._
-import org.slf4j.{ Logger, LoggerFactory }
+import org.slf4j.{Logger, LoggerFactory}
+import scalaj.http._
 
 object SkimlinksAPI {
 
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
 
-  def getDomains(apiKey: String, accountId: String): List[String] = {
-    val skimLinksDomainsUrl: String = s"https://merchants.skimapis.com/v3/domains"
+  def getAccessToken(clientId: String, clientSecret: String): Option[String] = {
+    val authResponse = Http(
+      url = "https://authentication.skimapis.com/access_token",
+    ).postData(
+      s"""{
+         | "client_id": "$clientId",
+         | "client_secret": "$clientSecret",
+         | "grant_type": "client_credentials"
+         |}""".stripMargin
+    ).asString
+
+    if (authResponse.isSuccess) {
+      parse(authResponse.body).map { authJson =>
+        authJson.hcursor.
+          downField("access_token").as[String]
+      }.flatMap { result =>
+        result
+      }.toOption
+    } else {
+      None
+    }
+  }
+
+  def getDomains(accessToken: String, publisherId: String): List[String] = {
+    val skimLinksDomainsUrl: String = s"https://merchants.skimapis.com/v4/publisher/$publisherId/domains"
 
     val domainsJson = Http(skimLinksDomainsUrl)
-      .param("apikey", apiKey)
-      .param("account_type", "publisher_admin")
-      .param("account_id", accountId)
+      .param("access_token", accessToken)
       .asString
 
     if (domainsJson.isSuccess) {
